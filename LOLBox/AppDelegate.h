4a339df3e70eaf6e2cf578c36017a342661864cc
g535879 <guyubinn@qq.com>
2015-11-09 17:07:01 +0800

    first commit

---
+++ LOLBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOLBox
+//
+//  Created by 古玉彬 on 15/11/9.
+//  Copyright © 2015年 guyubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
